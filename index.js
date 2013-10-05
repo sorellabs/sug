b913ae9e196e50d72580971797fd0527901fad50
Quildreen Motta <quildreen@gmail.com>
2013-10-04 23:40:18 -0300

    Documentation.

--- index.js
+++ index.js
@@ -34,12 +34,20 @@ var newlines = /\r\n|\r|\n/
 
 // ## Core implementation
 
+// ### Function: parse(text, language)
+// Parses text using the given language, returning a list of the tokens.
+//
+// Similar tokens are jammed together.
+//
 //- parse :: (String, Language) → [Token]
 function parse(text, language) {
   return text.split(newlines)
              .map(parseLine(language))
              .reduce(assimilateSimilarTokens, []) }
 
+// ### Function: render
+// Renders Markdown from a list of tokens.
+//
 //- render :: [Token] → String
 function render(tokens) {
   return tokens.map(renderToken)
@@ -47,8 +55,26 @@ function render(tokens) {
 
 
 // ## Languages
+//
+// Supported languages are defined here, and should follow the
+// `Language` interface. Since these use Boo, you can just derive from
+// similar languages to construct a new one.
+//
+// ### Type: Language
+//
+//     { "name"         : String                -- "Fenced" name of the language
+//     , "friendlyName" : String                -- The name displayed to the user
+//     , "extension"    : RegExp                -- RegExp matching the extension
+//     , "isEmpty"      : String → Boolean      -- Checks if line is whitespace
+//     , "isComment"    : String → Boolean      -- Checks if line is a comment
+//     , "parseComment" : String → Token        -- Parses comment into Token
+//     }
+
+
+// ### Pre-defined languages
 var languages = {}
 
+// Text languages
 languages.text = Base.derive({
   name: 'text',
   friendlyName: 'Plain text',
@@ -67,6 +93,7 @@ languages.text = Base.derive({
     return a }
 })
 
+// C-Family languages
 languages.c = languages.text.derive({
   name: 'c'
 , friendlyName: 'C'
@@ -93,6 +120,7 @@ languages.java = languages.c.derive({
   extension: /\.java$/
 })
 
+// Languages that use `#` as comment character
 languages.bash = Base.derive({
   name: 'bash'
 , friendlyName: 'Bash'
@@ -131,13 +159,18 @@ languages.livescript = languages.bash.derive({
   extension: /\.ls$/
 })
 
-// ## Helpes
 
+// ## Helpers
+
+// ### Function: parseLine(language)(line)
+//- parseLine :: Language → String → Token
 function parseLine(language) { return function(line) {
   return language.isEmpty(line)?    Blank.make()
   :      language.isComment(line)?  Text.make(language.parseComment(line))
   :      /* otherwise */            Code.make(line, language.name) }}
 
+// ### Function: assimilateSimilarTokens(xs, a)
+//- assimilateSimilarTokens :: ([Token], Token) → [Token]
 function assimilateSimilarTokens(xs, a) {
   var b  = last(xs)
   var ys = butLast(xs)
@@ -145,67 +178,107 @@ function assimilateSimilarTokens(xs, a) {
   :      /* _ */   ys.concat( b.canAssimilate(a)?  [b.assimilate(a)]
                             : /* otherwise */      [b, a]) }
 
+// ### Function: renderToken(token)
+//- renderToken :: Token → String
 function renderToken(token) {
   return token.render() }
 
+// ### Function: last(xs)
+//- last :: [a] → Maybe<a>
 function last(xs) {
   return xs[xs.length - 1] }
 
+// ### Function: butLast(xs)
+//- butLast :: [a] → [a]
 function butLast(xs) {
   return xs.slice(0, -1) }
 
 
 // ## Tokens
-var Token = Base.derive({
-  lastAssimilated: {}
+//
+// Tokens represent a line or a group of lines in a source (by way of
+// assimilating related tokens).
+//
+// A token is expected to fulfill the type:
+//
+//     { "canAssimilate" : Token → Boolean
+//     , "assimilate"    : Token → Boolean
+//     , "render"        : () → Boolean
+//     , "values"        : [a]
+//     , "type"          : String
+//     }
 
-, canAssimilate:
+
+// ### Object: Token
+var Token = Base.derive({
+  // #### Function: canAssimilate(other)
+  //- canAssimilate :: Token => Token → Boolean
+  canAssimilate:
   function _CanAssimilate(other) {
     return this.type === other.type
     ||     other.type === 'Blank' }
 
+  // #### Function: assimilate(other)
+  //- assimilate :: Token => Token → Token
 , assimilate:
   function _Assimilate(other) {
     this.values.push.apply(this.values, other.values)
     return this }
 
+  // #### Function: toString()
+  //- toString :: () → String
 , toString:
   function _ToString() {
     return '<' + this.type + ': ' + this.values.join('\n') + '>' }
 })
 
+// ### Object: Blank
 var Blank = Token.derive({
   type: 'Blank'
 
+  // #### Function: init()
+  //- init :: () → ()
 , init:
   function _init() {
     this.values = [' '] }
 
+  // #### Function: render()
+  //- render :: () → String
 , render:
   function _render() {
     return this.values.join('\n') }
 })
 
+// ### Object: Text
 var Text = Token.derive({
   type: 'Text'
 
+  // #### Function: init()
+  //- init :: () → ()
 , init:
   function _Init(value) {
     this.values = [value] }
 
+  // #### Function: render
+  //- render :: () → String
 , render:
   function _Render() {
     return this.values.join('\n') }
 })
 
+// ### Object: Code
 var Code = Token.derive({
   type: 'Code'
 
+  // #### Function: init
+  //- init :: () → ()
 , init:
   function _Init(value, language) {
     this.values   = [value]
     this.language = language }
 
+  // #### Function: render
+  //- render :: () → String
 , render:
   function _Render() {
     return !this.values.length?  ''
